03d98bc567e96628491c5c8b6fea7fe2bd413e0d
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-06-03 16:44:41 +0200

    More game more fun.

--- workflow/sandbox/toy_script.R
+++ workflow/sandbox/toy_script.R
@@ -25,6 +25,7 @@ library("dplyr")
 library("hrbrthemes")
 library("reshape2")
 library("ggrepel")
+library("tidyverse")
 
 library("ggh4x")
 library("ggrepel")
@@ -47,17 +48,10 @@ contributor_profiles <- read.csv(
   sep = ";")
 head(network_contribution)
 
-test <- network_contribution %>%
-  filter(
-    cmd == 12,
-    period == 1996,
-    country == "Japan"
-  )
-
 main_contributors <- network_contribution %>%
   filter(
     cmd == 12,
-    period %in% (2003:2022),
+    period %in% (2017:2022),
     contrib_trade_value_imp > quantile(.$contrib_trade_value_imp,
                                        probs = 0.95) |
       contrib_trade_value_exp > quantile(.$contrib_trade_value_imp,
@@ -69,7 +63,7 @@ main_contributors <- network_contribution %>%
 extract <- contributor_profiles %>%
   filter(
     cmd == 12,
-    period %in% (2003:2022),
+    period %in% (1996:2022),
     country %in% main_contributors
   )
 
@@ -77,7 +71,7 @@ grouping <- extract %>%
   mutate(edge_ratio = nb_edge_imp / nb_edge_exp) %>%
   group_by(country) %>%
   summarize(mean_edge_ratio = mean(edge_ratio, na.rm = TRUE)) %>%
-  mutate(group = ifelse(mean_edge_ratio < 0.5, "producers",
+  mutate(group = ifelse(mean_edge_ratio < 0.6, "producers",
                         ifelse(mean_edge_ratio > 2, "consumers",
                                "intermediates"))) %>%
   group_by(group)
@@ -91,46 +85,106 @@ country_groups <- list()
 for (group in names(grouping)) {
   country_groups[[group]] <- grouping[[group]]$country
 }
-country_groups
 
-plot_contribution <- network_contribution %>%
+plot_data <- network_contribution %>%
   filter(cmd == 12,
-         country %in% country_groups[[1]]) %>%
+         country %in% country_groups[[3]]) %>%
   mutate(contrib = (contrib_trade_value_exp +
-                      contrib_trade_value_imp) / 2) %>%
+                      contrib_trade_value_imp) / 2 * 100) %>%
   rowwise() %>%
-  mutate(contrib_min = min(contrib_trade_value_imp,
-                           contrib_trade_value_exp),
-         contrib_max = max(contrib_trade_value_imp,
-                           contrib_trade_value_exp)) %>%
+  mutate(contrib_min = (min(contrib_trade_value_imp,
+                            contrib_trade_value_exp) * 100),
+         contrib_max = (max(contrib_trade_value_imp,
+                            contrib_trade_value_exp) * 100))
+
+span <- .5
+plot_data_pred <- plot_data %>%
+  group_by(country) %>%
+  arrange(country, period) %>%
+  nest() %>%
+  mutate(
+    models = purrr::map(data,
+                        loess,
+                        formula = contrib ~ period,
+                        span = span),
+    contrib_pred = purrr::map(models, `[[`, "fitted")
+  ) %>%
+  select(-models) %>%
+  mutate(
+    models = purrr::map(data,
+                        loess,
+                        formula = contrib_min ~ period,
+                        span = span),
+    contrib_min_pred = purrr::map(models, `[[`, "fitted")
+  ) %>%
+  select(-models) %>%
+  mutate(
+    models = purrr::map(data,
+                        loess,
+                        formula = contrib_max ~ period,
+                        span = span),
+    contrib_max_pred = purrr::map(models, `[[`, "fitted")
+  ) %>%
+  select(-models) %>%
+  unnest(cols = c(data, contrib_pred, contrib_min_pred, contrib_max_pred))
+
+y_max <- max(plot_data_pred$contrib_max_pred, plot_data_pred$contrib_max)
+x_max <- max(plot_data_pred$period)
+
+plot_contribution <-  plot_data_pred %>%
 
   ggplot(aes(x = period,
-             y = contrib,
-             ymin = contrib_min,
-             ymax = contrib_max,
+             y = contrib_pred,
+             ymin = contrib_min_pred,
+             ymax = contrib_max_pred,
              group = country)
   ) +
-  geom_point(aes(x = period,
-                 y = contrib,
-                 shape = country,
-                 color = country)) +
   geom_line(aes(x = period,
-                y = contrib,
+                y = contrib_pred,
                 linetype = country,
                 color = country)) +
   geom_ribbon(aes(x = period,
-                  ymin = contrib_min,
-                  ymax = contrib_max,
+                  ymin = contrib_min_pred,
+                  ymax = contrib_max_pred,
                   fill = country),
-              alpha = 0.5)
+              alpha = 0.5) +
+  # geom_point(aes(x = period,
+  #                y = contrib_pred,
+  #                shape = country,
+  #                color = country)) +
+  geom_point(aes(x = period,
+                 y = contrib_min,
+                 shape = country,
+                 color = country),
+                 alpha = 0.3) +
+  geom_point(aes(x = period,
+                 y = contrib_max,
+                 shape = country,
+                 color = country),
+                 alpha = 0.3) +
+  geom_text(data = plot_data_pred %>% filter(period == x_max),
+            aes(label = country,
+                x = period + 1,
+                y = contrib_pred,
+                color = "black",
+                lineheight = .8,
+                fontface = "bold")) +
 
-plot_contribution %>%
-  filter(country == "Japan") %>%
-  select(period, country, contrib, contrib_min, contrib_max)
-
-network_contribution %>%
-  filter(country == "Japan", cmd == 12) %>%
-  select(period, country, contrib_trade_value_exp, contrib_trade_value_imp)
+  # Set up scale colors, breaks, and limits + themes
+  scale_color_grey() +
+  scale_fill_grey() +
+  scale_x_continuous(
+    breaks = c(1996, 2000, 2005, 2010, 2015, 2020, 2022),
+    labels = c("1996", "2000", "2005", "2010", "2015", "2020", "2022")
+  ) +
+  scale_y_continuous(limits = c(0, y_max),
+                     expand = c(0, 0)) +
+  labs(x = "Year",
+       y = "Contribution to global traded value (in %)") +
+  coord_cartesian(clip = "off") +
+  theme_ipsum(axis_title_size = 11) +
+  theme(legend.position = "none",
+        plot.margin = margin(20, 20, 20, 20))
 
 ggsave(
   "contribution.png",
@@ -146,126 +200,132 @@ ggsave(
   bg = "white"
 )
 
-pal <- c("#3D85F7", "#C32E5A")
 
-lst <- gsub("_exp", "", names(data)[grep("exp", names(data))])
-metrics <- lst[lst != "nb_edge"]
+#################
+# Function to shape data for plotting
+shape_data <- function(network_contribution,
+                       prod,
+                       time_span = 5,
+                       min_contrib = .05,
+                       span = .5) {
 
-myplots <- list()
+  # Get last year of the data
+  max_year <- max(network_contribution$period)
 
-for (metric in metrics) {
+  # Get the main contributors to trade in the last years
+  # (timespan years before last year)
+  main_contributors <- tibble::as.tibble(network_contribution) %>%
+    filter(
+      cmd == prod,  # nolint
+      period >= max_year - 5, # nolint
+      contrib_trade_value_imp > quantile(.$contrib_trade_value_imp, # nolint
+                                         probs = 1 - min_contrib) |
+      contrib_trade_value_exp > quantile(.$contrib_trade_value_imp, # nolint
+                                         probs = 1 - min_contrib)
+    ) %>%
+    select("country") %>%
+    {unique(c(.$country))} # nolint
 
-  # Select columns for plot
-  columns <- c("period", paste0(metric, c("_imp", "_exp")))
+  # Create value to plot: mean contribution, min contribution, max contribution
+  # and smooth values for nicer plotting
+  shape_data <- network_contribution %>%
+    filter(cmd == prod,
+           country %in% main_contributors) %>%  # nolint
+    mutate(contrib = (contrib_trade_value_exp +  # nolint
+                        contrib_trade_value_imp) / 2 * 100) %>%  # nolint
+    rowwise() %>%
+    mutate(contrib_min = (min(contrib_trade_value_imp,
+                              contrib_trade_value_exp) * 100),
+           contrib_max = (max(contrib_trade_value_imp,
+                              contrib_trade_value_exp) * 100))
 
-  # Define order for corresponding colors
-  order <- paste0(metric, c("_imp", "_exp"))
+  shape_data <- shape_data %>%  # nolint
+    group_by(country) %>%  # nolint
+    arrange(country, period) %>%  # nolint
+    nest() %>%  # nolint
+    mutate(
+      models = purrr::map(data,
+                          loess,
+                          formula = contrib ~ period,
+                          span = span),
+      contrib_pred = purrr::map(models, `[[`, "fitted")  # nolint
+    ) %>%
+    select(-models) %>%
+    mutate(
+      models = purrr::map(data,
+                          loess,
+                          formula = contrib_min ~ period,
+                          span = span),
+      contrib_min_pred = purrr::map(models, `[[`, "fitted")
+    ) %>%
+    select(-models) %>%
+    mutate(
+      models = purrr::map(data,
+                          loess,
+                          formula = contrib_max ~ period,
+                          span = span),
+      contrib_max_pred = purrr::map(models, `[[`, "fitted")
+    ) %>%
+    select(-models) %>%
+    unnest(cols = c(data, contrib_pred, contrib_min_pred, contrib_max_pred))  # nolint
 
-  # Define name for y axis
-  if ("mean" %in% metric){
-    name <- "Mean"
-  } else if ("var" %in% metric) {
-    name <- "Variance"
-  } else if ("skew" %in% metric) {
-    name <- "Skewness"
-  } else if ("kurt" %in% metric) {
-    name <- "Kurtosis"
-  }
-
-  # Produce basic plot
-  plot_connectivity <- data %>%
-    filter(cmd == 12) %>%
-    # Select relevant columns
-    select(all_of(columns)) %>%
-    # Convert wide to long dataframe
-    melt(id.vars = "period",
-         variable.name = "trade_flow",
-         value.name = "metric") %>%
-    arrange(period) %>%
-    # Convert trader_type to factor and order its values
-    mutate(trade_flow = factor(trade_flow, levels = order)) %>%
-
-    # Create ggplot line plot
-    ggplot(aes(x = period,
-               y = metric,
-               color = trade_flow,
-               label = trade_flow)) +
-    geom_point(alpha = 0.6) +
-    # geom_line(alpha = 0.6) +
-    geom_smooth(method = NULL,
-                se = FALSE,
-                linewidth = 1)
-
-  # Collect final y values of geom_smooth to plot legend label
-  y_imp <- filter(ggplot_build(plot_connectivity)$data[[2]],
-                  x == max(data$period) & grepl("_imp", label))$y
-  y_exp <- filter(ggplot_build(plot_connectivity)$data[[2]],
-                  x == max(data$period) & grepl("_exp", label))$y
-
-  # Collect y_min and y_max for setting axis limits
-  y_min <- min(min(ggplot_build(plot_connectivity)$data[[2]]$y),
-               min(select(filter(data, cmd == 12), contains(metric))))
-  y_max <- max(max(ggplot_build(plot_connectivity)$data[[2]]$y),
-               round(max(select(filter(data, cmd == 12), contains(metric)))))
-
-  # Complete plot with annotation and theme
-  plot_connectivity <- plot_connectivity +
-    # End of chart labels
-    annotate("text",
-             x = max(data$period) + 0.4,
-             y = y_imp,
-             label = "Imports",
-             hjust = 0,
-             size = 3,
-             lineheight = .8,
-             fontface = "bold",
-             color = pal[1]) +
-
-    annotate("text",
-             x = max(data$period) + 0.4,
-             y = y_exp,
-             label = "Exports",
-             hjust = 0,
-             size = 3,
-             lineheight = .8,
-             fontface = "bold",
-             color = pal[2]) +
-
-    # Set up scale colors, breaks, and limits + themes
-    scale_color_manual(values = pal) +
-    scale_x_continuous(
-      breaks = c(1996, 2000, 2005, 2010, 2015, 2020, 2022),
-      labels = c("1996", "2000", "2005", "2010", "2015", "2020", "2022")
-    ) +
-    scale_y_continuous(limits = c(trunc(y_min - 1),
-                                  round(y_max + 1)),
-                       expand = c(0, 0)) +
-    labs(x = "Year",
-         y = paste0(name,
-                    " of the nb. of trading partners")) +
-    coord_cartesian(clip = "off") +
-    theme_ipsum(axis_title_size = 11) +
-    theme(legend.position = "none",
-          plot.margin = margin(20, 20, 20, 20))
-
-  # Store plot in list
-  myplots[[match(metric, metrics)]] <- plot_connectivity
+  return(shape_data)
 }
 
-# Produce composite plot using patchwork
-final_plot <- myplots[[1]] + myplots[[2]] + myplots[[3]] + myplots[[4]] +
-  plot_layout(ncol = 2)
+grouping_countries <- function(network_contribution,
+                               contributor_profiles,
+                               prod,
+                               edge_ratio_min = 0.6,
+                               edge_ratio_max = 2) {
 
-ggsave(
-  "connectivity.png",
-  plot = final_plot,
-  device = "png",
-  path = "/Users/valentinmathieu/Desktop/",
-  scale = 1,
-  width = 2480 * 1.5,
-  height = 1240 * 2,
-  units = c("px"),
-  dpi = 300,
-  limitsize = TRUE,
-  bg = "white"
-)
+  # Extract first and last year of dataset
+  min_year <- min(network_contribution$period)
+  max_year <- max(network_contribution$period)
+
+  # Get trading countries in data
+  contributors <- network_contribution %>% distinct(country) # nolint
+
+  # Get contributors profiles
+  extract <- contributor_profiles %>%
+    filter(
+      cmd == prod,  # nolint
+      period %in% (min_year:max_year), # nolint
+      country %in% contributors$country  # nolint
+    )
+
+  # Group countries : producers, consumers, intermediates; based on their
+  # connections to import/export
+  grouping <- extract %>%
+    mutate(edge_ratio = nb_edge_imp / nb_edge_exp) %>%  # nolint
+    group_by(country) %>%  # nolint
+    summarize(mean_edge_ratio = mean(edge_ratio, na.rm = TRUE)) %>%  # nolint
+    mutate(group = ifelse(mean_edge_ratio < edge_ratio_min, "producers",  # nolint
+                          ifelse(mean_edge_ratio > edge_ratio_max, "consumers",
+                                 "intermediates"))) %>%
+    group_by(group)  # nolint
+  group_name <- group_keys(grouping)$group
+  grouping <- grouping %>%
+    group_by(group) %>%  # nolint
+    group_split() %>%
+    setNames(group_name)
+
+  country_groups <- list()
+  for (group in names(grouping)) {
+    country_groups[[group]] <- grouping[[group]]$country
+  }
+
+  return(country_groups)
+}
+
+test <- shape_data(network_contribution = network_contribution, prod = 12)
+groups <- grouping_countries(test, contributor_profiles, prod = 12)
+
+plot_lst <- list()
+for (group in country_groups) {
+
+  # Store plot in list
+  plot_lst[[which(sapply(country_groups,
+                         identical,
+                         group))]] <- group
+
+}
